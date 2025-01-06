2bd6fb23d2571053d48b9615fcac379d5dab2b4b
Matt Iakhno <matt.iakhno@gmail.com>
2025-01-05 18:25:19 -0800

    Docker image created for an Express API

---
+++ src/server.js
@@ -0,0 +1,24 @@
+//If you mess up Dockerfile ordering
+// you'll see long build times on every code change + build. If done correctly,
+// code changes should be only a few seconds to build locally due to build cache.
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const port = process.env.PORT;
+const secret = process.env.SECRET;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Express + TypeScript Server. Secret is: ' + secret);
+});
+
+app.get("/healthz", function (req, res) {
+  // do app logic here to determine if app is truly healthy
+  // you should return 200 if healthy, and anything else will fail
+  // if you want, you should be able to restrict this to localhost (include ipv4 and ipv6)
+  res.send("I am happy and healthy, port %s, secret %s \n", port, secret);
+});
+
+module.exports = app;
