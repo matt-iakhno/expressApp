3a78c6459662cc8c64393d2b17b9e0bb1e2e254b
Matt Iakhno <matt.iakhno@gmail.com>
2025-01-13 18:05:21 -0800

    Debugging run-tests

---
+++ api-node/jest.config.js
@@ -0,0 +1,10 @@
+/** @returns {Promise<import('jest').Config>} */
+module.exports = async () => {
+  return {
+    // verbose: true,
+    reporters: [
+      'default',
+      ['jest-ctrf-json-reporter', {}],
+    ],
+  };
+};
